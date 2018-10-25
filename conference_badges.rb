a9d84a834a4eceab40dd42a3aca707c884aa7985
Azraf Chowdhury <achowdhur@ltu.edu>
2018-10-25 03:41:07 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -6,5 +6,6 @@ end
 conference_speakers = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
 
 def batch_badge_creator(conference_speakers)
-  
+  conference_speakers.collect |name| return badgemaker()
+end
 end
