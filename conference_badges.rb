83b01879eaf85007884b4180ad6b6eed4c3b4e27
Azraf Chowdhury <achowdhur@ltu.edu>
2018-10-25 03:33:45 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+def badge_maker(speaker) 
+  puts "Hello, my name is #{speaker}."
+end
+
+conference_speakers = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+
+def batch_badge_creator(conference_speakers)
+  conference_speakers.collect {|speaker| badge_maker(speaker)}
+end
+
+def assign_rooms(conference_speakers)
+  conference_speakers.each_with_index.map {|speaker, map| puts "Hello, #{speaker}! You'll be assigned to room #{index+1}!"}
+end
+
+def printer(speakers)
+ batch_badge_creator(speakers).each do |badge|
+ puts badge
+end
+
+ assign_rooms(speakers).each do |assignment|
+  puts assignment
+ end
+end
