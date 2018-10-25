322a69eb569324741b0ebb9babf92593e7b95b2a
Azraf Chowdhury <achowdhur@ltu.edu>
2018-10-25 03:43:46 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,9 @@
 # Write your code here.
+conference_speakers = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
+
 def badge_maker(name) 
   return "Hello, my name is #{name}."
 end
-
-conference_speakers = ["Edsger","Ada","Charles","Alan","Grace","Linus","Matz"]
 
 def batch_badge_creator(conference_speaker)
   conference_speakers.collect {|conference_speaker| return badgemaker(conference_speakers)}
