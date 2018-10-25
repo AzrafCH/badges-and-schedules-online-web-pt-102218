61d33adfeb501deb2ea77f0e568a310ce760e81c
Azraf Chowdhury <achowdhur@ltu.edu>
2018-10-25 03:54:11 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -5,9 +5,9 @@ def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(array)
+def batch_badge_creator(conference_speakers)
   new_arr = []
-  array.each do |name|
+  conference_speakers.each do |name|
     new_arr.push("Hello, my name is #{name}.")
     end
   return new_arr
